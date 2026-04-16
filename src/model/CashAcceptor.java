4d74f425a3cbaad75659694d8755c25d4f3979bf
Yan_Asanaliev <chrom3owlyota@gmail.com>
2026-04-17 04:21:19 +0600

    redacting CashAcceptor class added getter, setter and cash deposit method

--- src/model/CashAcceptor.java
+++ src/model/CashAcceptor.java
@@ -1,4 +1,17 @@
 package model;
 
 public class CashAcceptor {
+    private int amount;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void deposit(int cash) {
+        this.amount += cash;
+    }
 }
